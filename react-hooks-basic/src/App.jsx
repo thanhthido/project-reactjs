85aeee12e331c92a43881a93456ccebcc241dc09
Thanh Thi <thidttpy@gmail.com>
2022-04-01 10:40:50 +0700

    Todo List

--- react-hooks-basic/src/App.jsx
+++ react-hooks-basic/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.scss'
-import ColorBox from './components/ColorBox'
+import TodoList from './components/TodoList'
 
 function App() {
+	const [todoList, setTodoList] = useState([
+		{ id: 1, title: 'I love Easy Frontend! 😍 ' },
+		{ id: 2, title: 'We love Easy Frontend! 🥰 ' },
+		{ id: 3, title: 'They love Easy Frontend! 🚀 ' },
+	])
+
+	function handleTodoClick(todo) {
+		console.log(todo)
+		const index = todoList.findIndex((x) => x.id === todo.id)
+		if (index < 0) return
+
+		const newTodoList = [...todoList]
+		newTodoList.splice(index, 1)
+		setTodoList(newTodoList)
+	}
+
 	return (
 		<div className='app'>
-			<h1>Welcome to React hooks</h1>
-			<ColorBox />
+			<h1>React hooks - TodoList</h1>
+			<TodoList todos={todoList} onTodoClick={handleTodoClick} />
 		</div>
 	)
 }
